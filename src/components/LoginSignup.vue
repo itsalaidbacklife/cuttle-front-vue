f30c9e7db057f359517486a7b7044f2d6b18bc9a
Ryan Emberling <itsalaidbacklife@gmail.com>
2020-08-13 08:11:39 -0400

    feat: placeholder for login page

---
+++ src/components/LoginSignup.vue
@@ -0,0 +1,18 @@
+<template>
+	<v-container>
+		Login Page Placeholder
+	</v-container>
+</template>
+
+<script>
+export default {
+	name: 'LoginSignup',
+	data: () => {
+
+	},
+}
+</script>
+
+<style lang="scss">
+	
+</style>
