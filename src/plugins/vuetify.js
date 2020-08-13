4a53fd881db67a5c9a9a083e8ee8381cf48c1429
Ryan Emberling <itsalaidbacklife@gmail.com>
2020-08-13 08:04:00 -0400

    style: add primary and secondary colors to theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,12 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+	theme: {
+		themes: {
+			light: {
+				primary: '#0093c4',
+				secondary: '#4CAF50',
+			},
+		},
+	},
 });
