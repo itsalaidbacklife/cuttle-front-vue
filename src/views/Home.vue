1ad2b8b2a71f40118999f7b53768a05772a8af59
Hao Li <haoli2@andrew.cmu.edu>
2020-09-07 20:50:01 -0400

    lint: empty line between styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -69,35 +69,33 @@ export default {
 	justify-content: center;
 	flex-direction: column;
 }
+
 #logo {
 	margin: 0 auto;
 }
+
 .page-title{
 	margin: 0 auto;
 	text-align: center;
 }
+
 #game-list-card{
 	border-radius: 15px;
 	padding: 45px;
 	-webkit-box-shadow: 1px 2px 6px 3px rgba(119,119,119,0.24) ;
 	box-shadow: 1px 2px 6px 3px rgba(119,119,119,0.24) ;
 }
+
 #game-list{
 	background-color: #efefef;
 }
+
 #home-card-title{
 	font-size: 2em;
 }
-#login-container button.v-btn {
-  padding: 0 32px 0;
-}
-#login-button-container {
-  position: relative;
-  display: flex;
-  justify-content: center;
-  width: 100%;
-}
+
 p {
   margin-top: 16px;
 }
+
 </style>
