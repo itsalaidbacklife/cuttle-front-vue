6e79dc2d65d177187685ab87f9528be6cce627f4
= <=>
2020-09-10 19:35:36 -0400

    test: add login specs

--- tests/e2e/specs/authentication.spec.js
+++ tests/e2e/specs/authentication.spec.js
@@ -1,8 +1,61 @@
 const validEmail = "myCustomEmail@gmail.com";
 const validPassword = "passwordLongerThanEight";
 
-describe('Logging In', () => {
+function assertSuccessfulAuth(email, password) {
+    cy.hash().should('eq', '#/');
+    // Check store.auth.authenticated and .email
+}
 
+function assertFailedAuth() {
+    cy.hash().should('eq', '#/login');
+    // Check store.auth.authenticated and .email
+}
+
+describe('Logging In', () => {
+    beforeEach(() => {
+        cy.wipeDatabase();
+        cy.visit('#/login');
+        cy.signup(validEmail, validPassword);
+    });
+
+    /**
+     * Successful Logins
+     */
+    it('Can log into existing account with submit button', () => {
+        cy.get('[data-cy=username]').type(validEmail);
+        cy.get('[data-cy=password]').type(validPassword);
+        cy.get('[data-cy=submit]').click();
+        // Should have redirected to homepage
+        // cy.hash().should('eq', '#/');
+        assertSuccessfulAuth();
+    });
+    it('Can login via enter key in email', () => {
+        cy.get('[data-cy=password]').type(validPassword);
+        cy.get('[data-cy=username]').type(validEmail + '{enter}');
+        cy.hash().should('eq', '#/');
+    });
+    it('Can login via enter key in password', () => {
+        cy.get('[data-cy=username]').type(validEmail);
+        cy.get('[data-cy=password]').type(validPassword + '{enter}');
+        cy.hash().should('eq', '#/');
+    });
+
+    /**
+     * Rejected logins
+     */
+    it('Rejects login before signup', () => {
+        cy.get('[data-cy=username]').type('unRegisteredEmail@aol.gov');
+        cy.get('[data-cy=password]').type(validPassword);
+        cy.get('[data-cy=submit]').click();
+        assertFailedAuth();
+    });
+
+    it('Rejects incorrect password', () => {
+        cy.get('[data-cy=username]').type(validEmail);
+        cy.get('[data-cy=password]').type('incorrectPw');
+        cy.get('[data-cy=submit]').click();
+        assertFailedAuth();
+    });
 });
 
 describe('Signing Up', () => {
