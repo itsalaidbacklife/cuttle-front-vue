023b0445937629bc39d5b8b06defd06fe7674483
= <=>
2020-09-13 17:01:18 -0400

    feat: basic layout for Lobby

---
+++ src/components/LobbyPlayerIndicator.vue
@@ -0,0 +1,47 @@
+<template>
+	<div
+		class="player-indicator"
+		:class="{'ready': playerReady}"
+	>
+		<h3>
+			{{ message }}
+		</h3>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'LobbyPlayerIndicator',
+	props: {
+		playerEmail: {
+			type: String,
+			default: null,
+		},
+		playerReady: {
+			type: Boolean,
+			default: false,
+		}
+	},
+	computed: {
+		message() {
+			return this.playerEmail || 'Invite'
+		}
+	}
+}
+</script>
+
+<style>
+.player-indicator {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    height: 200px;
+    width: 200px;
+    background-color: #C4C4C4;
+    padding: 16px;
+}
+.ready {
+    border: 4px solid #4CAF50;
+}
+</style>
--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -1,11 +1,57 @@
 <template>
-	<div id="lobby-wrapper">
-		<h1>Lobby for {{ gameId }}</h1>
-	</div>
+	<v-container id="lobby-wrapper">
+		<v-row>
+			<v-col offset="1">
+				<h1>Lobby for {{ gameId }}</h1>
+			</v-col>
+		</v-row>
+		<v-row>
+			<v-col
+				cols="3"
+				offset="1"
+			>
+				<v-btn
+					outlined
+					color="primary"
+				>
+					EXIT
+				</v-btn>
+			</v-col>
+			<v-spacer />
+			<v-col cols="3">
+				<v-btn
+					contained
+					color="primary"
+					@click="iAmReady = !iAmReady"
+				>
+					READY
+				</v-btn>
+			</v-col>
+		</v-row>
+		<!-- Usernames -->
+		<v-row>
+			<v-col offset="1">
+				<lobby-player-indicator
+					:player-email="$store.state.auth.email"
+					:player-ready="iAmReady"
+				/>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 <script>
+import LobbyPlayerIndicator from '../components/LobbyPlayerIndicator';
+
 export default {
 	name: 'Lobby',
+	components: {
+		LobbyPlayerIndicator,
+	},
+	data() {
+		return {
+			iAmReady: false,
+		}
+	},
 	computed: {
 		gameId() {
 			return this.$store.state.game.gameId;
