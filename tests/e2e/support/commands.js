10af09ea9bf808cc00a645c777976b89b35dcbbf
= <=>
2020-09-16 08:11:22 -0400

    lint:autofix

--- tests/e2e/support/commands.js
+++ tests/e2e/support/commands.js
@@ -7,54 +7,54 @@ io.sails.useCORSRouteToGetCookie = false;
 
 Cypress.Commands.add('wipeDatabase', () => {cy.request('localhost:1337/test/wipeDatabase')});
 Cypress.Commands.add('signup', (email, password) => {
-    return new Promise((resolve, reject) => {
-        io.socket.get('localhost:1337/user/signup',
-        {
-            email,
-            password
-        }, function(res, jwres) {
-            if (jwres.statusCode !== 200) {
-                return reject(new Error('Failed to sign up via command'));
-            }
-            return resolve(res);
-        });
-    });
+	return new Promise((resolve, reject) => {
+		io.socket.get('localhost:1337/user/signup',
+			{
+				email,
+				password
+			}, function(res, jwres) {
+				if (jwres.statusCode !== 200) {
+					return reject(new Error('Failed to sign up via command'));
+				}
+				return resolve(res);
+			});
+	});
 });
 Cypress.Commands.add('signupThroughStore', (email, password) => {
-    cy.window().its('app.$store').invoke('dispatch', 'requestSignup', {email, password});
+	cy.window().its('app.$store').invoke('dispatch', 'requestSignup', {email, password});
 });
 Cypress.Commands.add('loginThroughStore', (email, password) => {
-    cy.window().its('app.$store').invoke('dispatch', 'requestLogin', {email, password});
+	cy.window().its('app.$store').invoke('dispatch', 'requestLogin', {email, password});
 });
 Cypress.Commands.add('vueRoute', (route) => {
-    cy.window().its('app.$router').invoke('push', route);
+	cy.window().its('app.$router').invoke('push', route);
 });
 Cypress.Commands.add('createGame', (name) => {
-    return new Promise((resolve, reject) => {
-        io.socket.post('/game/create', {
-            gameName: name
-        }, function handleResponse(resData, jwres) {
-            if (jwres.statusCode === 200) {
-                return resolve(resData);
-            }
-            return reject(new Error('Error creating game'));
-        });
-    })
+	return new Promise((resolve, reject) => {
+		io.socket.post('/game/create', {
+			gameName: name
+		}, function handleResponse(resData, jwres) {
+			if (jwres.statusCode === 200) {
+				return resolve(resData);
+			}
+			return reject(new Error('Error creating game'));
+		});
+	})
 });
 Cypress.Commands.add('createGameThroughStore', (name) => {
-    return cy.window().its('app.$store').invoke('dispatch', 'requestCreateGame', name);
+	return cy.window().its('app.$store').invoke('dispatch', 'requestCreateGame', name);
 });
 Cypress.Commands.add('subscribeOtherUser', (id) => {
-    return new Promise((resolve, reject) => {
-        io.socket.get('/game/subscribe', {
-            id,
-        }, function handleResponse(res, jwres) {
-            if (jwres.statusCode === 200) {
-                return resolve();
-            }
-            return reject(new Error('error subscribing'));
-        });
-    });
+	return new Promise((resolve, reject) => {
+		io.socket.get('/game/subscribe', {
+			id,
+		}, function handleResponse(res, jwres) {
+			if (jwres.statusCode === 200) {
+				return resolve();
+			}
+			return reject(new Error('error subscribing'));
+		});
+	});
 });
 
 
