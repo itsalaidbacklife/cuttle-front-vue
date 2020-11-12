8983b9c0292da9784cb1ff3591e8f7078543582c
= <=>
2020-11-12 08:22:17 -0500

    chore: add neutral colors to theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -10,6 +10,18 @@ export default new Vuetify({
 				primary: "#6202EE",
 				secondary: "#FD6222",
 				accent: '#219653',
+				neutral: {
+					lighten5: '#FAFBFC',
+					lighten4: '#FAFAFA',
+					lighten3: '#F2F4F5',
+					lighten2: '#E6E9EB',
+					lighten1: '#CDD1D4',
+					base: '#A3ADB5',
+					darken1: '#7D8C97',
+					darken2: '#607280',
+					darken3: '#3D505E',
+					darken4: '#2A3740',
+				  },
 			}
 		}
 	}
