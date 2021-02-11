dd551b856a0da8d14f0209a87ef9d70592b8148b
= <=>
2021-02-10 20:15:01 -0500

    test: test playing points now tests both DOM and store state using new assertGameState() helper

--- tests/e2e/specs/game.spec.js
+++ tests/e2e/specs/game.spec.js
@@ -1,3 +1,5 @@
+import { assertGameState } from '../support/helpers';
+
 const validEmail = 'myCustomEmail@gmail.com';
 const validPassword = 'passwordLongerThanEight';
 const opponentEmail = 'yourMortalEnemy@cia.gov';
@@ -39,11 +41,14 @@ describe('Game - Page Content', () => {
 			}
 		)
 			.then(() => {
+				/////////
+				// DOM //
+				/////////
 				// Test initial score
 				cy.get('#player-score')
 					.should('contain', 'POINTS: 10')
 					.should('contain', 'TARGET: 14');
-				// Play points	
+				// Play points (ace of spades)
 				cy.get('[data-cy=1-3]').click(); // ace of spades
 				cy.get('#player-field')
 					.should('have.class', 'valid-move')
@@ -53,6 +58,19 @@ describe('Game - Page Content', () => {
 				cy.get('#player-score')
 					.should('contain', 'POINTS: 11')
 					.should('contain', 'TARGET: 14');
+
+				// Test store state
+				assertGameState(
+					{
+						// ace of spades moved from p0Hand to p0Points
+						p0Hand: [{suit: 0, rank: 1}],
+						p0Points: [{suit: 3, rank: 10}, {suit: 3, rank: 1}],
+						p0FaceCards: [{suit: 3, rank: 13}],
+						p1Hand: [{suit: 2, rank: 1}, {suit: 1, rank: 1}],
+						p1Points: [{suit: 2, rank: 10}],
+						p1FaceCards: [{suit: 2, rank: 13}],
+					}
+				);
 			});
 	})
 })
