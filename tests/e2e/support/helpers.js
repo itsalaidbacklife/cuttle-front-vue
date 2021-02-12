c36558e595011edbfd79ed7b1c24af0e3e6db54d
= <=>
2021-02-11 19:18:32 -0500

    chore: add helpers to stringify cards

--- tests/e2e/support/helpers.js
+++ tests/e2e/support/helpers.js
@@ -1,3 +1,70 @@
+export function hasValidSuitAndRank(card) {
+    if (!Object.prototype.hasOwnProperty.call(card, 'rank')) return false;
+    if (!Object.prototype.hasOwnProperty.call(card, 'suit')) return false;
+    if (!Number.isInteger(card.rank)) return false;
+    if (!Number.isInteger(card.suit)) return false;
+    if (card.rank < 1 || card.rank > 13) return false;
+    if (card.suit < 0 || card.suit > 3) return false;
+    return true;
+}
+/**
+ * @returns string name of card
+ * @param {suit: number, rank: number} card 
+ */
+export function printCard(card) {
+    if (!hasValidSuitAndRank(card)) {
+        throw new Error('Cannot print object not shaped like card');
+    }
+    let res = '';
+    switch (card.rank) {
+        case 1:
+            res += 'Ace'
+            break;
+        case 2:
+            res += 'Two'
+            break;
+        case 3:
+            res += 'Three'
+            break;
+        case 4:
+            res += 'Four'
+            break;
+        case 5:
+            res += 'Five'
+            break;
+        case 6:
+            res += 'Six'
+            break;
+        case 7:
+            res += 'Seven'
+            break;
+        case 8:
+            res += 'Eight'
+            break;
+        case 9:
+            res += 'Nine'
+            break;
+        case 10:
+            res += 'Ten'
+            break;
+        case 11:
+            res += 'Jack'
+            break;
+        case 12:
+            res += 'Queen'
+            break;
+        case 13:
+            res += 'King'
+            break;
+    }
+    res += ' of ';
+    const suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
+    res += suits[card.suit];
+    return res;
+}
+export function printCardList(cards) {
+    return cards.map(printCard).join(', ');
+}
 export function cardsMatch(card1, card2) {
 	return card1.rank === card2.rank && card1.suit === card2.suit;
 }
@@ -68,7 +135,7 @@ export function assertGameState(fixture) {
             expect(cardListsMatch(game.players[0].points, fixture.p0Points)).to.eq(true, 'P0 Points should match fixture');
             expect(cardListsMatch(game.players[0].runes, fixture.p0FaceCards)).to.eq(true, 'P0 Face Cards should match fixture');
             // Player 1
-            expect(cardListsMatch(game.players[1].hand, fixture.p1Hand)).to.eq(true, 'P1 Hand should match fixture');
+            expect(cardListsMatch(game.players[1].hand, fixture.p1Hand)).to.eq(true, `P1 Hand should match fixture -- actual: ${printCardList(game.players[1].hand)} did not match ficture: ${printCardList(fixture.p1Hand)}`);
             expect(cardListsMatch(game.players[1].points, fixture.p1Points)).to.eq(true, 'P1 Points should match fixture');
             expect(cardListsMatch(game.players[1].runes, fixture.p1FaceCards)).to.eq(true, 'P1 Face Cards should match fixture');
 		});
