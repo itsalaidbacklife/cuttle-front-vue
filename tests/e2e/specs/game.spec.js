de1cbf2105f5341d05f3cc9c263a4c9118edb741
= <=>
2021-02-13 07:58:21 -0500

    chore: remove unnecessary .then() in play points test

--- tests/e2e/specs/game.spec.js
+++ tests/e2e/specs/game.spec.js
@@ -43,55 +43,53 @@ describe('Game - Basic Moves', () => {
 				p1FaceCards: [{suit: 2, rank: 13}],
 			}
 		)
-			.then(() => {
-				// Test initial score
-				cy.get('#player-score')
-					.should('contain', 'POINTS: 10')
-					.should('contain', 'TARGET: 14');
-				// Play points (ace of spades)
-				cy.get('[data-player-hand-card=1-3]').click(); // ace of spades
-				cy.get('#player-field')
-					.should('have.class', 'valid-move')
-					.click()
-					.should('not.have.class', 'valid-move');
-				// Attempt to play out of turn
-				cy.get('[data-player-hand-card=1-0]').click(); // ace of clubs
-				cy.get('#player-field')
-					.click();
-				// Error snackbar says its not your turn
-				cy.get('[data-cy=game-snackbar] .v-snack__wrapper')
-					.should('be.visible')
-					.should('have.class', 'error')
-					.should('contain', "It's not your turn");
+		// Test initial score
+		cy.get('#player-score')
+			.should('contain', 'POINTS: 10')
+			.should('contain', 'TARGET: 14');
+		// Play points (ace of spades)
+		cy.get('[data-player-hand-card=1-3]').click(); // ace of spades
+		cy.get('#player-field')
+			.should('have.class', 'valid-move')
+			.click()
+			.should('not.have.class', 'valid-move');
+		// Attempt to play out of turn
+		cy.get('[data-player-hand-card=1-0]').click(); // ace of clubs
+		cy.get('#player-field')
+			.click();
+		// Error snackbar says its not your turn
+		cy.get('[data-cy=game-snackbar] .v-snack__wrapper')
+			.should('be.visible')
+			.should('have.class', 'error')
+			.should('contain', "It's not your turn");
 
-				assertGameState(
-					0,
-					{
-						// ace of spades moved from p0Hand to p0Points
-						p0Hand: [{suit: 0, rank: 1}],
-						p0Points: [{suit: 3, rank: 10}, {suit: 3, rank: 1}],
-						p0FaceCards: [{suit: 3, rank: 13}],
-						p1Hand: [{suit: 2, rank: 1}, {suit: 1, rank: 1}],
-						p1Points: [{suit: 2, rank: 10}],
-						p1FaceCards: [{suit: 2, rank: 13}],
-					}
-				);
+		assertGameState(
+			0,
+			{
+				// ace of spades moved from p0Hand to p0Points
+				p0Hand: [{suit: 0, rank: 1}],
+				p0Points: [{suit: 3, rank: 10}, {suit: 3, rank: 1}],
+				p0FaceCards: [{suit: 3, rank: 13}],
+				p1Hand: [{suit: 2, rank: 1}, {suit: 1, rank: 1}],
+				p1Points: [{suit: 2, rank: 10}],
+				p1FaceCards: [{suit: 2, rank: 13}],
+			}
+		);
 
-				// Opponent plays the ace of diamonds
-				cy.playPointsOpponent({rank: 1, suit: 1});
+		// Opponent plays the ace of diamonds
+		cy.playPointsOpponent({rank: 1, suit: 1});
 
-				assertGameState(
-					0,
-					{
-						// ace of diamonds moved from p1Hand to p1Points
-						p0Hand: [{suit: 0, rank: 1}],
-						p0Points: [{suit: 3, rank: 10}, {suit: 3, rank: 1}],
-						p0FaceCards: [{suit: 3, rank: 13}],
-						p1Hand: [{suit: 2, rank: 1}],
-						p1Points: [{suit: 2, rank: 10}, {suit: 1, rank: 1}],
-						p1FaceCards: [{suit: 2, rank: 13}],
-					}
-				);
-			});
+		assertGameState(
+			0,
+			{
+				// ace of diamonds moved from p1Hand to p1Points
+				p0Hand: [{suit: 0, rank: 1}],
+				p0Points: [{suit: 3, rank: 10}, {suit: 3, rank: 1}],
+				p0FaceCards: [{suit: 3, rank: 13}],
+				p1Hand: [{suit: 2, rank: 1}],
+				p1Points: [{suit: 2, rank: 10}, {suit: 1, rank: 1}],
+				p1FaceCards: [{suit: 2, rank: 13}],
+			}
+		);
 	})
 })
