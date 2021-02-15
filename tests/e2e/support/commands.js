5d749b374bbf2287057e282aa8b12ff711c4016a
= <=>
2021-02-14 21:17:33 -0500

    chore: add scuttleOpponent() command

--- tests/e2e/support/commands.js
+++ tests/e2e/support/commands.js
@@ -138,6 +138,40 @@ Cypress.Commands.add('playPointsOpponent', (card) => {
 			});
 	});
 });
+/**
+ * @param card {suit: number, rank: number}
+ * @param target {suit: number, rank: number}
+ */
+Cypress.Commands.add('scuttleOpponent', (card, target) => {
+	if (!hasValidSuitAndRank(card)) {
+		throw new Error('Cannot scuttle as opponent: Invalid card input');
+	}
+	return cy
+		.window()
+		.its('app.$store.state.game')
+		.then((game) => {
+			const player = game.players[game.myPNum];
+			const opponent = game.players[(game.myPNum + 1) % 2];
+			const foundCard = opponent.hand.find((handCard) => cardsMatch(card, handCard));
+			const foundTarget = player.points.find((pointCard) => cardsMatch(target, pointCard));
+			if (!foundCard) {
+				throw new Error(`Error scuttling as opponent: could not find ${card.rank} of ${card.suit} in opponent hand`);
+			}
+			if (!foundTarget) {
+				throw new Error(`Error scuttling as opponent: could not find ${target.rank} of ${target.suit} in player's points`);
+			}
+			io.socket.get('/game/scuttle', {
+				opId: player.id,
+				cardId: foundCard.id,
+				targetId: foundTarget.id,
+			}, function handleResponse(res, jwres) {
+				if (!jwres.statusCode === 200) {
+					throw new Error(jwres.body.message);
+				}
+				return jwres;
+			});
+		});
+});
 Cypress.Commands.add('vueRoute', (route) => {
 	cy.window()
 		.its('app.$router')
