e379b9bbe80889fd265fe8d9f35cea2b9190c6b4
= <=>
2021-02-21 11:57:25 -0500

    [WIP] feat: begin implementation of cannot counter dialog

---
+++ src/components/GameView/CannotCounterDialog.vue
@@ -0,0 +1,56 @@
+<template>
+	<v-dialog
+		v-model="show"
+	>
+		<v-card id="cannot-counter-dialog">
+			<v-card-title>Cannot Counter</v-card-title>
+			<v-card-text>
+				<!-- Your opponent has played the {{ oneOff.name }} as a one-off. You cannot counter, because you do not have a two.
+				<card
+					:suit="oneOff.suit"
+					:rank="oneOff.rank"
+				/> -->
+			</v-card-text>
+			<v-card-actions class="d-flex justify-end">
+				<v-btn
+					data-cy="resolve"
+					color="primary"
+				>
+					Resolve
+				</v-btn>
+			</v-card-actions>
+		</v-card>
+	</v-dialog>
+</template>
+
+<script>
+import Card from '@/components/GameView/Card.vue';
+
+export default {
+	name: 'CannotCounterDialog',
+	components: {
+		// Card,
+	},
+	props: {
+		value: {
+			type: Boolean,
+			required: true,
+		},
+		oneOff: {
+			required: true,
+		}
+	},
+	computed: {
+		show: {
+			get() {
+				return this.value;
+			},
+			set(val) {
+				this.$emit('input', val);
+			}
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped></style>
