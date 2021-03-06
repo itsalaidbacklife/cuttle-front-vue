498dda1fe7a7489980b62395d2ffbad6c4f4470f
= <=>
2021-03-06 11:58:55 -0500

    test: add test for playing 8 for points

--- tests/e2e/specs/game.spec.js
+++ tests/e2e/specs/game.spec.js
@@ -305,7 +305,7 @@ describe('Game Basic Moves - P0 Perspective', () => {
 		);
 
 		// Attempt to play queen out of turn
-		cy.get('[data-player-hand-card=12-1]').click(); // queen of clubs
+		cy.get('[data-player-hand-card=12-1]').click(); // queen of diamonds
 		cy.get('#player-field')
 			.should('have.class', 'valid-move')
 			.click();
@@ -325,6 +325,51 @@ describe('Game Basic Moves - P0 Perspective', () => {
 				scrap: [],
 			}
 		);
+	});
+	
+	it.only('Plays eights for points', () => {
+		// Setup
+		cy.loadGameFixture({
+			p0Hand: [Card.EIGHT_OF_SPADES, Card.EIGHT_OF_HEARTS, Card.KING_OF_CLUBS, Card.QUEEN_OF_DIAMONDS],
+			p0Points: [Card.TEN_OF_HEARTS],
+			p0FaceCards: [],
+			p1Hand: [Card.SIX_OF_HEARTS, Card.QUEEN_OF_HEARTS],
+			p1Points: [Card.ACE_OF_DIAMONDS],
+			p1FaceCards: [],
+		});
+		cy.get('[data-player-hand-card]').should('have.length', 4);
+		cy.log('Loaded fixture');
+
+		// Player plays eight
+		cy.get('[data-player-hand-card=8-3]').click(); // eight of spades
+		cy.get('#player-field')
+			.should('have.class', 'valid-move')
+			.click();
+		// Choose to play for points
+		cy.get('#eight-overlay')
+			.should('be.visible')
+			.get('[data-cy=eight-for-points]')
+			.click();
+		
+		assertGameState(
+			0,
+			{
+				p0Hand: [Card.EIGHT_OF_HEARTS, Card.KING_OF_CLUBS, Card.QUEEN_OF_DIAMONDS],
+				p0Points: [Card.TEN_OF_HEARTS, Card.EIGHT_OF_SPADES],
+				p0FaceCards: [],
+				p1Hand: [Card.SIX_OF_HEARTS, Card.QUEEN_OF_HEARTS],
+				p1Points: [Card.ACE_OF_DIAMONDS],
+				p1FaceCards: [],
+			}
+		);
+		
+		// Attempt to play eight out of turn
+		// Player plays eight
+		cy.get('[data-player-hand-card=8-2]').click(); // eight of hearts
+		cy.get('#player-field')
+			.should('have.class', 'valid-move')
+			.click();
+		assertSnackbarError('It\'s not your turn');
 	});
 });
 
