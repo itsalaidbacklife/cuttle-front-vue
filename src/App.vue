84294fac02edf061aaf1d6d2a122a734cfddca28
= <=>
2021-03-11 07:51:51 -0500

    style: switch font family to PT Serif

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,10 @@
 </template>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Berkshire+Swash&display=swap");
-@import url("https://fonts.googleapis.com/css2?family=BioRhyme:wght@200;300;400;700;800&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=PT+Serif:wght@400;700&display=swap');
 
 #app {
-  font-family: "BioRhyme", serif;
+  font-family: 'PT Serif', serif;
 }
 
-h1,
-h2 {
-  font-family: "Berkshire Swash", cursive;
-}
 </style>
