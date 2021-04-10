5d701e4f7c8c018d28428558c22b50d795e7c8a5
= <=>
2021-04-10 14:04:13 -0400

    feat: enable selecting cards to discard

--- src/components/GameView/FourDialog.vue
+++ src/components/GameView/FourDialog.vue
@@ -13,11 +13,13 @@
 				<!-- Cards in hand -->
 				<div class="d-flex flex-wrap">
 					<card
-						v-for="card in hand"
+						v-for="(card, index) in hand"
 						:key="card.id"
 						:suit="card.suit"
 						:rank="card.rank"
 						:data-discard-card="`${card.rank}-${card.suit}`"
+						:class="{'is-selected': selectedIds.includes(card.id)}"
+						@click="selectCard(index)"
 					/>
 				</div>
 			</v-card-text>
@@ -49,7 +51,7 @@ export default {
 	},
 	data() {
 		return {
-			selectedIndices: [],
+			selectedIds: [],
 		}
 	},
 	computed: {
@@ -65,5 +67,26 @@ export default {
 			return this.$store.state.game.players[this.$store.state.game.myPNum].hand;
 		},
 	},
+	methods: {
+		selectCard(handIndex) {
+			const cardId = this.hand[handIndex].id;
+			// If already selected, deselect
+			if (this.selectedIds.includes(cardId)) {
+				this.selectedIds.splice(this.selectedIds.indexOf(cardId), 1);
+			} else {
+				this.selectedIds.push(cardId);
+				// If three cards are selected, deselect 1st selection
+				if (this.selectedIds.length > 2) {
+					this.selectedIds.splice(0, 1);
+				}
+			}
+		}
+	},
 }
 </script>
+
+<style lang="scss" scoped>
+	.is-selected {
+		border: 2px solid red;
+	}
+</style>
