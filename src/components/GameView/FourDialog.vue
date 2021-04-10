88bd57614f103f0c67d6e097aa18e1600e708df2
= <=>
2021-04-10 14:09:33 -0400

    style: apply theme style to selected cards for discard

--- src/components/GameView/FourDialog.vue
+++ src/components/GameView/FourDialog.vue
@@ -18,7 +18,7 @@
 						:suit="card.suit"
 						:rank="card.rank"
 						:data-discard-card="`${card.rank}-${card.suit}`"
-						:class="{'is-selected': selectedIds.includes(card.id)}"
+						:class="{'selected': selectedIds.includes(card.id)}"
 						@click="selectCard(index)"
 					/>
 				</div>
@@ -86,7 +86,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-	.is-selected {
-		border: 2px solid red;
+	.selected {
+		border: 3px solid var(--v-error-base);
 	}
 </style>
