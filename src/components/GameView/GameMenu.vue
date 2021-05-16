c9e838f7cd5b5fb7a5e0f96ec9afc9e83c929051
= <=>
2021-05-16 10:35:28 -0400

    fix: close game menu when opening concede dialog (reimplemented to avoid nested activators)

--- src/components/GameView/GameMenu.vue
+++ src/components/GameView/GameMenu.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-menu :close-on-content-click="true">
+	<v-menu>
 		<!-- Activator -->
 		<template #activator="{ on, attrs }">
 			<v-btn
@@ -15,38 +15,40 @@
 		<!-- Menu -->
 		<v-list id="game-menu">
 			<!-- Concede Dialog (Initiate + Confirm) -->
-			<v-dialog>
-				<template #activator="dialogActivator">
-					<v-list-item
-						v-bind="dialogActivator.attrs"
-						data-cy="concede-initiate"
-						v-on="dialogActivator.on"
-					>
-						Concede
-					</v-list-item>
-				</template>
-				<v-card id="concede-menu">
-					<v-card-title>Concede?</v-card-title>
-					<v-card-text>
-						The game will end and your opponent will win.
-					</v-card-text>
-					<v-card-actions class="d-flex justify-end">
-						<v-btn text color="primary">
-							Cancel
-						</v-btn>
-						<v-btn color="error" depressed data-cy="concede-confirm" @click="concede">
-							Concede
-						</v-btn>
-					</v-card-actions>
-				</v-card>
-			</v-dialog>
+			<v-list-item
+				data-cy="concede-initiate"
+				@click="showConcedeDialog = true"
+			>
+				Concede
+			</v-list-item>
 		</v-list>
+		<v-dialog v-model="showConcedeDialog">
+			<v-card id="concede-menu">
+				<v-card-title>Concede?</v-card-title>
+				<v-card-text>
+					The game will end and your opponent will win.
+				</v-card-text>
+				<v-card-actions class="d-flex justify-end">
+					<v-btn text color="primary" data-cy="concede-cancel" @click="showConcedeDialog = false">
+						Cancel
+					</v-btn>
+					<v-btn color="error" depressed data-cy="concede-confirm" @click="concede">
+						Concede
+					</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
 	</v-menu>
 </template>
 
 <script>
 export default {
 	name: 'GameMenu',
+	data() {
+		return {
+			showConcedeDialog: false,
+		};
+	},
 	methods: {
 		concede() {
 			this.$store.dispatch('requestConcede');
