b2ba712465d2712af3126b8cf59aaf03a240af6d
= <=>
2021-05-23 16:28:39 -0400

    feat: activate score goal tooltip via clicking info icon instead of hovering over goal

--- src/components/GameView/ScoreGoalToolTip.vue
+++ src/components/GameView/ScoreGoalToolTip.vue
@@ -1,38 +1,42 @@
 <template>
-	<v-menu
-		open-on-hover
-		:top="isPlayer"
-		:bottom="!isPlayer"
-		offset-y
-	>
-		<template #activator="{on, attrs}">
-			<span
-				v-bind="attrs"
-				class="ml-4"
-				:data-cy="dataCyName"
-				v-on="on"
-			>
-				GOAL: {{ pointsToWin }}
-			</span>
-		</template>
-		<v-list class="score-goal-explanation">
-			<v-list-item :class="{'current-goal': kingCount === 0}">
-				0 Kings: 21pts
-			</v-list-item>
-			<v-list-item :class="{'current-goal': kingCount === 1}">
-				1 King:  14pts
-			</v-list-item>
-			<v-list-item :class="{'current-goal': kingCount === 2}">
-				2 Kings: 10pts
-			</v-list-item>
-			<v-list-item :class="{'current-goal': kingCount === 3}">
-				3 Kings: 7pts
-			</v-list-item>
-			<v-list-item :class="{'current-goal': kingCount === 4}">
-				4 Kings: 5pts
-			</v-list-item>
-		</v-list>
-	</v-menu>
+	<span>
+		<span
+			v-bind="attrs"
+			class="ml-4"
+			:data-cy="dataCyName"
+			v-on="on"
+		>
+			GOAL: {{ pointsToWin }}
+		</span>
+		<v-menu
+			:top="isPlayer"
+			:bottom="!isPlayer"
+			offset-y
+		>
+			<template #activator="{on, attrs}">
+				<v-btn class="mb-2" x-small icon v-bind="attrs" v-on="on">
+					<v-icon color="white" small>mdi-information</v-icon>
+				</v-btn>
+			</template>
+			<v-list class="score-goal-explanation">
+				<v-list-item :class="{'current-goal': kingCount === 0}">
+					0 Kings: 21pts
+				</v-list-item>
+				<v-list-item :class="{'current-goal': kingCount === 1}">
+					1 King:  14pts
+				</v-list-item>
+				<v-list-item :class="{'current-goal': kingCount === 2}">
+					2 Kings: 10pts
+				</v-list-item>
+				<v-list-item :class="{'current-goal': kingCount === 3}">
+					3 Kings: 7pts
+				</v-list-item>
+				<v-list-item :class="{'current-goal': kingCount === 4}">
+					4 Kings: 5pts
+				</v-list-item>
+			</v-list>
+		</v-menu>
+	</span>
 </template>
 
 <script>
