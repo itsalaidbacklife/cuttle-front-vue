da792139a000d9a194c9838795742c992cb6de5c
Hao Li <lihaobhsfer@gmail.com>
2021-06-24 18:15:38 -0400

    lint: fix

--- tests/e2e/specs/game/reconnecting.spec.js
+++ tests/e2e/specs/game/reconnecting.spec.js
@@ -1,7 +1,7 @@
 import { setupGameAsP0, validEmail, validPassword, assertGameState, Card } from '../../support/helpers';
 describe('Reconnecting to a game', () => {
-    it('Reconnects after refreshing the page', () => {
-        setupGameAsP0();
+	it('Reconnects after refreshing the page', () => {
+		setupGameAsP0();
 
 		cy.loadGameFixture({
 			p0Hand: [Card.ACE_OF_CLUBS],
@@ -13,33 +13,33 @@ describe('Reconnecting to a game', () => {
 		});
 		cy.get('[data-player-hand-card]')
 			.should('have.length', 1);
-        cy.log('Fixture loaded');
+		cy.log('Fixture loaded');
 
-        // Reload page, relogin
-        cy.reload();
-        cy.get('#reauthenticate-dialog')
-            .should('be.visible');
-        cy.get('[data-cy=username]').type(validEmail);
-        cy.get('[data-cy=password]').type(validPassword);
-        cy.get('[data-cy=login]').click();
-        cy.get('#reauthenticate-dialog')
-            .should('not.be.visible');
-        cy.log('Reauthenticated');
+		// Reload page, relogin
+		cy.reload();
+		cy.get('#reauthenticate-dialog')
+			.should('be.visible');
+		cy.get('[data-cy=username]').type(validEmail);
+		cy.get('[data-cy=password]').type(validPassword);
+		cy.get('[data-cy=login]').click();
+		cy.get('#reauthenticate-dialog')
+			.should('not.be.visible');
+		cy.log('Reauthenticated');
 
-        // // Play Ace of Clubs for points
-        cy.get('[data-player-hand-card=1-0]').click();
+		// // Play Ace of Clubs for points
+		cy.get('[data-player-hand-card=1-0]').click();
 		cy.get('#player-field')
 			.should('have.class', 'valid-move')
 			.click()
-            .should('not.have.class', 'valid-move');
+			.should('not.have.class', 'valid-move');
         
-        assertGameState(0, {
+		assertGameState(0, {
 			p0Hand: [],
 			p0Points: [Card.SEVEN_OF_DIAMONDS, Card.SEVEN_OF_HEARTS, Card.ACE_OF_CLUBS],
 			p0FaceCards: [],
 			p1Hand: [],
 			p1Points: [],
 			p1FaceCards: [],
-        });
-    });
+		});
+	});
 });
