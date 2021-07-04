18794a44ee2f069f09b586afea66499a2e3ca219
= <=>
2021-07-04 11:30:58 -0400

    test: implement test case for countering after reconnecting

--- tests/e2e/specs/game/reconnecting.spec.js
+++ tests/e2e/specs/game/reconnecting.spec.js
@@ -1,4 +1,16 @@
 import { setupGameAsP0, setupGameAsP1, validEmail, validPassword, assertGameState, Card } from '../../support/helpers';
+
+function reconnect() {
+	cy.get('#reauthenticate-dialog')
+		.should('be.visible');
+	cy.get('[data-cy=username]').type(validEmail);
+	cy.get('[data-cy=password]').type(validPassword);
+	cy.get('[data-cy=login]').click();
+	cy.get('#reauthenticate-dialog')
+		.should('not.be.visible');
+	cy.log('Reauthenticated');
+}
+
 describe('Reconnecting to a game', () => {
 	it('Reconnects after refreshing the page', () => {
 		setupGameAsP0();
@@ -17,14 +29,7 @@ describe('Reconnecting to a game', () => {
 
 		// Reload page, relogin
 		cy.reload();
-		cy.get('#reauthenticate-dialog')
-			.should('be.visible');
-		cy.get('[data-cy=username]').type(validEmail);
-		cy.get('[data-cy=password]').type(validPassword);
-		cy.get('[data-cy=login]').click();
-		cy.get('#reauthenticate-dialog')
-			.should('not.be.visible');
-		cy.log('Reauthenticated');
+		reconnect();
 
 		// // Play Ace of Clubs for points
 		cy.get('[data-player-hand-card=1-0]').click();
@@ -66,14 +71,7 @@ describe('Reconnecting to a game', () => {
 		// Reload page
 		cy.reload();
 		// Reauthenticate
-		cy.get('#reauthenticate-dialog')
-			.should('be.visible');
-		cy.get('[data-cy=username]').type(validEmail);
-		cy.get('[data-cy=password]').type(validPassword);
-		cy.get('[data-cy=login]').click();
-		cy.get('#reauthenticate-dialog')
-			.should('not.be.visible');
-		cy.log('Reauthenticated');
+		reconnect();
 
 		// Cannot counter dialog appears again
 		cy.get('#cannot-counter-dialog')
@@ -88,6 +86,46 @@ describe('Reconnecting to a game', () => {
 			p1Hand: [Card.ACE_OF_DIAMONDS],
 			p1Points: [],
 			p1FaceCards: [Card.ACE_OF_CLUBS, Card.SEVEN_OF_DIAMONDS, Card.SEVEN_OF_HEARTS, Card.SIX_OF_CLUBS],
+		});
+	});
+
+	it.only('Can counter after reconnecting', () => {
+		setupGameAsP1();
+
+		cy.loadGameFixture({
+			p0Hand: [Card.ACE_OF_CLUBS],
+			p0Points: [Card.SEVEN_OF_DIAMONDS, Card.SEVEN_OF_HEARTS],
+			p0FaceCards: [],
+			p1Hand: [Card.TWO_OF_CLUBS],
+			p1Points: [Card.ACE_OF_DIAMONDS],
+			p1FaceCards: [],
+		});
+		cy.get('[data-player-hand-card]')
+			.should('have.length', 1);
+		cy.log('Fixture loaded');
+
+		cy.playOneOffOpponent(Card.ACE_OF_CLUBS);
+
+		cy.get('#counter-dialog')
+			.should('be.visible');
+		
+		cy.reload();
+		reconnect();
+
+		cy.get('#counter-dialog')
+			.should('be.visible')
+			.get('[data-cy=counter]')
+			.click();
+		
+		cy.resolveOpponent();
+		assertGameState(1, {
+			p0Hand: [],
+			p0Points: [Card.SEVEN_OF_DIAMONDS, Card.SEVEN_OF_HEARTS],
+			p0FaceCards: [],
+			p1Hand: [],
+			p1Points: [Card.ACE_OF_DIAMONDS],
+			p1FaceCards: [],
+			scrap: [Card.ACE_OF_CLUBS, Card.TWO_OF_CLUBS],
 		});
 	});
 });
