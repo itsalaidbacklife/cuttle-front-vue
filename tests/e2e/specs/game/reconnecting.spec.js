154b393d6a3cb60e28f67fdb65c50f1643bbaa96
= <=>
2021-07-04 11:19:06 -0400

    test: add failing test case for cannot counter dialog after reconnecting

--- tests/e2e/specs/game/reconnecting.spec.js
+++ tests/e2e/specs/game/reconnecting.spec.js
@@ -1,4 +1,4 @@
-import { setupGameAsP0, validEmail, validPassword, assertGameState, Card } from '../../support/helpers';
+import { setupGameAsP0, setupGameAsP1, validEmail, validPassword, assertGameState, Card } from '../../support/helpers';
 describe('Reconnecting to a game', () => {
 	it('Reconnects after refreshing the page', () => {
 		setupGameAsP0();
@@ -41,5 +41,44 @@ describe('Reconnecting to a game', () => {
 			p1Points: [],
 			p1FaceCards: [],
 		});
+	});
+
+	it.only('Allows countering a one-off after reconnecting', () => {
+		setupGameAsP1();
+
+		cy.loadGameFixture({
+			p0Hand: [Card.ACE_OF_CLUBS],
+			p0Points: [Card.SEVEN_OF_DIAMONDS, Card.SEVEN_OF_HEARTS],
+			p0FaceCards: [],
+			p1Hand: [Card.ACE_OF_DIAMONDS],
+			p1Points: [Card.SIX_OF_CLUBS],
+			p1FaceCards: [],
+		});
+		cy.get('[data-player-hand-card]')
+			.should('have.length', 1);
+		cy.log('Fixture loaded');
+
+		cy.playOneOffOpponent(Card.ACE_OF_CLUBS);
+
+		cy.get('#cannot-counter-dialog')
+			.should('be.visible');
+		
+		// Reload page
+		cy.reload();
+		// Reauthenticate
+		cy.get('#reauthenticate-dialog')
+			.should('be.visible');
+		cy.get('[data-cy=username]').type(validEmail);
+		cy.get('[data-cy=password]').type(validPassword);
+		cy.get('[data-cy=login]').click();
+		cy.get('#reauthenticate-dialog')
+			.should('not.be.visible');
+		cy.log('Reauthenticated');
+
+		// Cannot counter dialog appears again
+		cy.get('#cannot-counter-dialog')
+			.should('be.visible')
+			.get('[data-cy=cannot-counter-resolve]')
+			.click();
 	});
 });
