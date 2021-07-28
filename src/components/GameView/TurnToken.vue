3acfeaa2c22b066e2731be8df79a479c295919b2
= <=>
2021-07-28 09:35:11 -0400

    chore: implement TurnToken component

---
+++ src/components/GameView/TurnToken.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-expand-transition>
+        <v-card
+            v-if="show"
+            id="turn-token"
+            rounded="circle"
+            height="32"
+            width="32"
+            elevation="3"
+            class="mx-2"
+        >
+            <v-img :src="require('@/assets/logo_head.svg')" />
+        </v-card>
+    </v-expand-transition>
+</template>
+
+<script>
+export default {
+    name: 'TurnToken',
+    props: {
+        show: {
+            type: Boolean,
+            required: true,
+        },
+    },
+}
+</script>
