b70c79592a65e7bef7a84b3ee95dd0df24175a3d
Ryan Emberling <itsalaidbacklife@gmail.com>
2021-12-01 10:19:41 -0500

    test: update 6 spec to include a double-jacked point card in addition to the two single-jacked point cards

--- tests/e2e/specs/in_game/oneOffs.spec.js
+++ tests/e2e/specs/in_game/oneOffs.spec.js
@@ -89,17 +89,25 @@ describe('Untargeted One-Offs', () => {
 		// Setup
 		cy.loadGameFixture({
 			//Player is P0
-			p0Hand: [Card.ACE_OF_CLUBS, Card.SIX_OF_SPADES, Card.SIX_OF_DIAMONDS, Card.JACK_OF_CLUBS],
+			p0Hand: [Card.ACE_OF_CLUBS, Card.SIX_OF_SPADES, Card.SIX_OF_DIAMONDS, Card.JACK_OF_CLUBS, Card.JACK_OF_HEARTS],
 			p0Points: [Card.THREE_OF_SPADES, Card.ACE_OF_SPADES],
 			p0FaceCards: [Card.KING_OF_SPADES, Card.KING_OF_CLUBS, Card.KING_OF_DIAMONDS],
 			// Opponent is P1
-			p1Hand: [Card.ACE_OF_HEARTS, Card.JACK_OF_DIAMONDS],
-			p1Points: [Card.TEN_OF_HEARTS, Card.ACE_OF_DIAMONDS],
+			p1Hand: [Card.ACE_OF_HEARTS, Card.JACK_OF_DIAMONDS, Card.JACK_OF_SPADES],
+			p1Points: [Card.TWO_OF_HEARTS, Card.ACE_OF_DIAMONDS],
 			p1FaceCards: [Card.KING_OF_HEARTS],
 		});
-		cy.get('[data-player-hand-card]').should('have.length', 4);
+		cy.get('[data-player-hand-card]').should('have.length', 5);
 		cy.log('Loaded fixture');
 
+		// Double Jack (control will remain unchanged when 6 resolves)
+		// Player & Opponent jack then re-jack the TWO of Hearts
+		cy.get('[data-player-hand-card=11-2]').click();
+		cy.get('[data-opponent-point-card=2-2]').click();
+		cy.get('[data-player-point-card=2-2]');
+		cy.playJackOpponent(Card.JACK_OF_SPADES, Card.TWO_OF_HEARTS);
+
+		// Single Jacks (control will switch when 6 resolves)
 		// Player jacks opponent's Ace of diamonds
 		cy.get('[data-player-hand-card=11-0]').click();
 		cy.get('[data-opponent-point-card=1-1]').click();
@@ -116,7 +124,7 @@ describe('Untargeted One-Offs', () => {
 				p0FaceCards: [],
 				// Opponent is P1
 				p1Hand: [Card.ACE_OF_HEARTS],
-				p1Points: [Card.TEN_OF_HEARTS, Card.ACE_OF_DIAMONDS],
+				p1Points: [Card.TWO_OF_HEARTS, Card.ACE_OF_DIAMONDS],
 				p1FaceCards: [],
 				scrap:[
 					Card.SIX_OF_SPADES,
@@ -126,6 +134,8 @@ describe('Untargeted One-Offs', () => {
 					Card.KING_OF_SPADES,
 					Card.JACK_OF_CLUBS,
 					Card.JACK_OF_DIAMONDS,
+					Card.JACK_OF_HEARTS,
+					Card.JACK_OF_SPADES,
 				]
 			}
 		);
