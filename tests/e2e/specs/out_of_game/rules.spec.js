1332ed718ec0bbdf1eba2ac95b36120ce077e02f
Ryan Emberling <itsalaidbacklife@gmail.com>
2022-03-18 12:00:51 -0400

    chore: newlines

--- tests/e2e/specs/out_of_game/rules.spec.js
+++ tests/e2e/specs/out_of_game/rules.spec.js
@@ -2,12 +2,14 @@ import {
 	username,
 	validPassword
 } from '../../support/helpers';
+
 describe('Rules Page', () => {
 	beforeEach(() => {
 		cy.wipeDatabase();
 		cy.visit('/');
 		cy.vueRoute('/rules');
 	});
+
 	it('Top Home button - Navigates to Login when unauthenticated and home when authenticated', () => {
 		cy.get('[data-cy=top-home-button]').click();
 		cy.hash().should('eq', '#/login');
@@ -17,6 +19,7 @@ describe('Rules Page', () => {
 		cy.get('[data-cy=top-home-button]').click();
 		cy.hash().should('eq', '#/');
 	});
+
 	it('Bottom Home button - Navigates to Login when unauthenticated and home when authenticated', () => {
 		cy.get('[data-cy=bottom-home-button]').click();
 		cy.hash().should('eq', '#/login');
